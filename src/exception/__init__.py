8ed0a54b6a44974f6a30bbf1ea5dbd71a3c6d6c2
Mohammad Aaban <mohammadaaban4@gmail.com>
2026-07-10 22:20:36 +0530

    feat:add custom exception handling module

---
+++ src/exception/__init__.py
@@ -0,0 +1,15 @@
+import os
+
+def error_message_detail(error,error_detail):
+    _,_,exc_tb = error_detail.exc_info()
+    file_name = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+    line_number = exc_tb.tb_lineno
+    return f"Error occurred in python script name [{file_name}] line number [{line_number}] error message [{str(error)}]"
+
+class RentException(Exception):
+    def __init__(self,error_message,error_detail):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message,error_detail=error_detail)
+
+    def __str__(self):
+        return self.error_message
